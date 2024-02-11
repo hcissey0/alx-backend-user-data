50bdaeecff4787411f623cc50e3758240506cdee
Hassan Cissey <hcissey0@gmail.com>
2024-02-10 21:23:53 -0800

    filtered

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,5 @@ import re
 
 def filter_datum(fields, redaction, message, separator):
     """this will do the work"""
-    for field in fields:
-        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+    [(m := re.sub(f"{f}=[^;]*", f"{f}={redaction}", message) for f in fields]
     return message
