1c745cf905ce6e1a7eb5d96080a6dcf0ed9beadd
Hassan Cissey <hcissey0@gmail.com>
2024-02-16 02:49:59 +0000

    Basic Auth - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,3 +53,20 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> tuple[str, str]:
+        """_summary_
+
+        Args:
+            self (_type_): _description_
+            str (_type_): _description_
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        tok = decoded_base64_authorization_header.split(":")
+        return tok[0], tok[1]
