2f2b7eebd266f9814a2490403f922c8bca651d27
Hassan Cissey <hcissey0@gmail.com>
2024-02-16 03:23:37 +0000

    Basic Auth - Overload current_user and BOOM!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-auth_type = getenv('AUTH_TYPE', 'basic_auth')
+auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
