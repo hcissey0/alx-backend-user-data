04431d64ff7c0c12e7c1f069527d179e58f9a37c
Hassan Cissey <hcissey0@gmail.com>
2024-02-16 02:27:17 +0000

    Basic Auth - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,20 @@ class BasicAuth(Auth):
     Args:
         Auth (_type_): _description_
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """_summary_
+
+        Args:
+            authorization_header (str): _description_
+
+        Returns:
+            str: _description_
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.removeprefix("Basic ")
