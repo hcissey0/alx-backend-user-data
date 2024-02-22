53dcfeae134657a4f0b6718ecc1c58d7576249a0
Hassan Cissey <hcissey0@gmail.com>
2024-02-22 15:38:25 +0000

    Get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,5 +73,20 @@ def profile() -> str:
     abort(403)
 
 
+@app.route("/reset_password", methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """This is the route that return the reset password token
+
+    Returns:
+        str: a JSON
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": f"{email}", "reset_token": f"{reset_token}"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
